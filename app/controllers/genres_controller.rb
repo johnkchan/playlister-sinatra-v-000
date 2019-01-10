d5e3edc29c12a5a804473b1e47af1ea92308fdb5
Jonathan Chan <johnkchan252@gmail.com>
2019-01-10 03:10:46 +0000

    Automatically backed up by Learn

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -1,15 +1,11 @@
 class GenresController < ApplicationController
-
-    get '/genres' do
-        @genres = Genre.all
-        erb :'/genres/index'
-    end
-
-    get '/genres/:slug' do
-        @genre = Genre.find_by_slug(params[:slug])
-        @songs = @genre.songs
-        @artists = @genre.artists
-        erb :'/genres/show'
-    end
-
+  get '/genres' do 
+    @genres = Genre.all
+    erb :'genres/index'
+  end
+  
+  get '/genres/:slug' do
+    @genre = Genre.find_by_slug(params[:slug])
+    erb :'/genres/show'
+  end
 end
